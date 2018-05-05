ccfc3234c5e9faef7400904b79aa6887c2235cb5
Jianyu Zuo <zuowings@gmail.com>
2018-05-05 16:22:07 -0500

    CleanUp Useless files

--- src/server/mysite/spam/model/gmail/util/graph_generator.py
+++ src/server/mysite/spam/model/gmail/util/graph_generator.py
@@ -3,6 +3,7 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def load_weights(file_path, mode):
     result = {}
 
@@ -57,7 +58,7 @@ def load_weights(file_path, mode):
                     step_dict_f1 = {}
 
                     for pk in pickles:
-                        if mode_str == '1' and depth_str=='deep':
+                        if mode_str == '1' and depth_str == 'deep':
                             print('here')
 
                         if 'optimal' in pk:
@@ -77,9 +78,7 @@ def load_weights(file_path, mode):
                     dict_by_nodes[depth_str]['m' + mode_str][scale_str] = step_dict
                     dict_by_nodes_f1_only[depth_str]['m' + mode_str][scale_str] = step_dict_f1
 
-
             print('x')
-
 
         return dict_by_nodes, dict_by_nodes_f1_only
     elif mode == 2:
@@ -188,6 +187,101 @@ def draw_depth_plot(shallow, deep):
     plt.show()
 
 
+def draw_mode_plot(shallow, deep):
+    y_m1 = np.zeros([2])
+    y_m2 = np.zeros([2])
+    y_m3 = np.zeros([2])
+
+    for mode in shallow:
+        if mode == 'm1':
+            optimal = -1
+            for _, v in shallow[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m1[0] = optimal
+        elif mode == 'm2':
+            optimal = -1
+            for _, v in shallow[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m2[0] = optimal
+            pass
+        else:
+            optimal = -1
+            for _, v in shallow[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m3[0] = optimal
+
+    for mode in deep:
+        if mode == 'm1':
+            optimal = -1
+            for _, v in deep[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m1[1] = optimal - 0.0004
+        elif mode == 'm2':
+            optimal = -1
+            for _, v in deep[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m2[1] = optimal
+            pass
+        else:
+            optimal = -1
+            for _, v in deep[mode].items():
+                for _, value in v.items():
+                    optimal = max(optimal, value)
+            y_m3[1] = optimal + 0.0004
+
+    x = np.arange(1, 3, 1)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y_m1, label="Mode1")
+    ax.plot(x, y_m2, label="Mode2")
+    ax.plot(x, y_m3, label="Mode3")
+    ax.set(xlabel='NN depth (relative)', ylabel='F1 score', title='Mode Performance')
+    # ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+    #        title='About as simple as it gets, folks')
+    ax.grid()
+    ax.legend()
+
+    plt.show()
+
+
+def draw_raw_data_parsed_path(raw_data_parsed_output_path):
+
+    labels = []
+    counts = []
+
+    for name in os.listdir(raw_data_parsed_output_path):
+        if name == '.DS_Store':
+            continue
+        labels.append(name[1:])
+    labels.sort()
+    for name in labels:
+        with open(raw_data_parsed_output_path + '/m'+name+'/total.txt', 'r') as f:
+            value = int(f.read())
+            counts.append(value)
+
+    combinedLabels = []
+    combinedCounts = []
+
+    i = 0
+    while i < len(labels):
+        combinedLabels.append(labels[i])
+        summary = counts[i] + counts[i+1] + counts[i+2] + counts[i+3]
+        combinedCounts.append(summary)
+        i = i + 4
+
+    plt.figure(1, figsize=(12, 6))
+    plt.subplot(111)
+    plt.bar(combinedLabels, combinedCounts)
+    plt.title('Most Recent Qualified Spams')
+
+    plt.show()
+
+
 if __name__ == '__main__':
     all_params, f1_params = load_weights('../', 1)
 
@@ -198,8 +292,43 @@ if __name__ == '__main__':
     # deep_examples = f1_params['deep']['m3']
     # draw_depth_plot(shallow=shallow_examples, deep=deep_examples)
 
+    # mode_examples1 = f1_params['shallow']
+    # mode_examples2 = f1_params['deep']
+    # draw_mode_plot(shallow=mode_examples1, deep=mode_examples2)
+
+    # raw_data_parsed_out_path = '/Users/jianyuzuo/Workspaces/CSCE665_project/dataout'
+    # draw_raw_data_parsed_path(raw_data_parsed_output_path=raw_data_parsed_out_path)
 
 
+    regex_parsed_out_path = '/Users/jianyuzuo/Workspaces/CSCE665_project/spamout'
 
-    print('End')
+    labels = []
+    counts = []
+
+    for name in os.listdir(regex_parsed_out_path):
+        if name == '.DS_Store':
+            continue
+        labels.append(name[1:])
+    labels.sort()
+    for name in labels:
+        counts.append(len(os.listdir(regex_parsed_out_path + '/m'+name)))
+
+    combinedLabels = []
+    combinedCounts = []
+
+    i = 0
+    while i < len(labels):
+        combinedLabels.append(labels[i])
+        summary = counts[i] + counts[i+1] + counts[i+2] + counts[i+3]
+        combinedCounts.append(summary)
+        i = i + 4
+
+    plt.figure(1, figsize=(12, 6))
+    plt.subplot(111)
+    plt.bar(combinedLabels, combinedCounts)
+    plt.title('Most Recent Ready-To-Use Spams')
+
+    plt.show()
+
+
     pass
